650a5360c7a6108a4d524012db2dace3eb4412c4
irinap0n0mareva <64708950+irinap0n0mareva@users.noreply.github.com>
2020-06-08 01:41:17 +0500

    Initial commit

---
+++ lab7/BinaryTree.h
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "TreeNode.h"
+
+using namespace std;
+
+class BinaryTree {
+    TreeNode *root;
+
+    void FreeMemory(TreeNode* node);
+    TreeNode* Insert(TreeNode *node, int value);
+    TreeNode* Search(TreeNode *node, int value);
+    void Print(TreeNode *node, int level) const;
+public:
+    BinaryTree();
+
+    void Insert(int value);
+    TreeNode* Search(int value);
+
+    void Print() const;
+    ~BinaryTree();
+};
